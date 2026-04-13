adae42a809fbe4b3daba6010ff265423291b149c
Kudakwashe Mike Mapaya <kudamapaya@gmail.com>
2026-04-13 10:26:50 +0200

    feat:add superuser

--- build.sh
+++ build.sh
@@ -5,3 +5,11 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py loaddata monitoring_sites
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'kudamapaya@gmail.com', 'kuda9477')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
