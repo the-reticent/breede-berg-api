1f701b086b447b4c4d3f0597a21b66dde17cbbef
Kudakwashe Mike Mapaya <kudamapaya@gmail.com>
2026-04-14 12:05:29 +0200

    feat: add iNaturalist sync, seed fixtures for wildlife, water quality and vegetation data

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py loaddata monitoring_sites
+python manage.py loaddata wildlife_sightings
+python manage.py loaddata water_quality_readings
+python manage.py loaddata vegetation_surveys
